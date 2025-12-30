b931fd79672837e3845ca76339ffab1845d8bf03
Artem Morozov <morozec76@gmail.com>
2025-12-30 22:35:11 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use redis::{aio::MultiplexedConnection, Client, RedisResult};
+
+#[tokio::main]
+async fn main() -> RedisResult<()> {
+    let client = Client::open("redis://127.0.0.1/")?;
+    let mut conn: MultiplexedConnection = client.get_multiplexed_async_connection().await?;
+
+    let stream = "events:in";
+
+    let entries: Vec<(String, Vec<(String,String)>)> = redis::cmd("XRANGE")
+        .arg(stream)
+        .arg("-")
+        .arg("+")
+        .query_async(&mut conn)
+        .await?;
+
+    for (id, fields) in entries {
+        println!("id: {}", id);
+        for (k, v) in fields {
+            println!("  {}: {}", k, v);
+        }
+    } 
+
+    Ok(())
+}
